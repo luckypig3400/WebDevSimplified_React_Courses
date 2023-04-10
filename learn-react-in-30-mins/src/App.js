83a9518e2d1d2e758dc34f60fddc9750701f909b
郵差 <twHenryYang@gmail.com>
2023-04-10 21:51:19 +0800

    feat: React useRef & onClick event first try

--- learn-react-in-30-mins/src/App.js
+++ learn-react-in-30-mins/src/App.js
@@ -1,14 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import TodoList from './TodoList';
 
 function App() {
-  const [myTodos, setTodos] = useState([{ id: 1, name: 'Todo 1', complete: true }, { id: 2, name: 'Todo 2', complete: false }]);
+  const [myTodos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if(name === '') return;// do nothing
+    console.log(`You have added a new Todo: ${name}`);
+  }
 
   return (
     <>
       <TodoList todos={myTodos} />
-      <input type="text" /><br />
-      <button>Add Todo</button>
+      <input ref={todoNameRef} type="text" /><br />
+      <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear All</button>
       <div>0 left to do</div>
     </>
