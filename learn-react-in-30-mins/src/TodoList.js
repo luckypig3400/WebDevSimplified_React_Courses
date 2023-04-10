8d1238e1c2d546460256aa831f082cf7929ee86c
郵差 <twHenryYang@gmail.com>
2023-04-10 21:34:32 +0800

    feat: Make each element in array a component

--- learn-react-in-30-mins/src/TodoList.js
+++ learn-react-in-30-mins/src/TodoList.js
@@ -1,12 +1,11 @@
 import React from 'react'
 // 輸入rfc，VSCode安裝的React套件會自動產生空的react function component程式碼
+import Todo from './Todo';
 
 export default function TodoList({ todos }) {
   return (
-    <div>
-      <h1>Todo List</h1>
-
-      {todos.length}
-    </div>
+    todos.map(todo => {
+      return <Todo key={todo} todo={todo} />
+    })
   )
 }
