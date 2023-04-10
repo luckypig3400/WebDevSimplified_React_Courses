afabb9dc4549129750fd51e7f52c4acb4c6484f3
郵差 <twHenryYang@gmail.com>
2023-04-10 22:18:15 +0800

    feat: useEffect & localStorage first try

--- learn-react-in-30-mins/src/App.js
+++ learn-react-in-30-mins/src/App.js
@@ -1,11 +1,26 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList';
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 // https://stackoverflow.com/questions/62549457/error-err-package-path-not-exported-package-subpath-v4-is-not-defined-by
+
+const LOCAL_STORAGE_KEY = 'todoApp.todosSavedItems';
 
 function App() {
   const [myTodos, setTodos] = useState([]);
   const todoNameRef = useRef();
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+    // 這個useEffect() function因為沒有監聽的變數
+    // 因此只會在頁面載入時執行一次
+  }, [])
+
+  useEffect(() => {
+    console.log("這個useEffect() function會在每次myTodos變數有更動時執行");
+
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(myTodos));
+  }, [myTodos])
 
   function handleAddTodo(e) {
     const name = todoNameRef.current.value;
